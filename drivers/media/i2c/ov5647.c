a4b961c6559e3a605ff6df98a46531dbb99ed9b8
qiuTony629 <zyq2793025956@gmail.com>
2025-07-23 11:08:19 +0800

    set ov5647 default mode 2

--- drivers/media/i2c/ov5647.c
+++ drivers/media/i2c/ov5647.c
@@ -626,7 +626,7 @@ static const struct ov5647_mode ov5647_modes[] = {
 
 /* Default sensor mode is 2x2 binned 640x480 SBGGR10_1X10. */
 #define OV5647_DEFAULT_MODE	(&ov5647_modes[2])
-#define OV5647_DEFAULT_FORMAT	(ov5647_modes[2].format)
+#define OV5647_DEFAULT_FORMAT	(ov5647_modes[0].format)
 
 static int ov5647_write16(struct v4l2_subdev *sd, u16 reg, u16 val)
 {
